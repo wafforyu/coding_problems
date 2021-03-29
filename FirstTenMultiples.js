258349bf08f9fc22bcf1e846f0fa8e377696d34e
wafforyu <wafforyu@gmail.com>
2021-03-29 23:26:17 +0800

    added java version for first ten multiples

--- FirstTenMultiples.js
+++ FirstTenMultiples.js
@@ -8,3 +8,16 @@ function tenMultiples(n){
     }
     return null;
 }
+
+
+//java
+/**
+ public static void tenMultiples(int n){
+    int a = 1;
+    int p = n;
+    while(a <= 10){
+        p =  n * a;
+        System.out.println(p);
+        a++;
+    }
+ */
