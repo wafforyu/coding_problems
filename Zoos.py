5182678ee3d9dcd5e791b8105e40960cc94d1622
wafforyu <72655919+wafforyu@users.noreply.github.com>
2021-04-03 23:57:19 +0800

    Update Zoos.py

--- Zoos.py
+++ Zoos.py
@@ -1,7 +1,5 @@
 #You are required to enter a word that consists of  and  that denote the number of Zs and Os respectively. The input word is considered similar to word zoo if .
-#
 #Determine if the entered word is similar to word zoo.
-#
 #For example, words such as zzoooo and zzzoooooo are similar to word zoo but not the words such as zzooo and zzzooooo.
 
 string = input("enter zoo: ")
@@ -19,5 +17,4 @@ def zoos(string):
         print("Yes")
     else:
         print("No")
-
 zoos(string)
