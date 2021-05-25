7c1640c027e840a43aab99679caf84d13be86b9a
wafforyu <wafforyu@gmail.com>
2021-05-25 19:18:08 +0800

    add barking dog

---
+++ BarkingDog.java
@@ -0,0 +1,8 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        if (hourOfDay < 0 || hourOfDay > 23) return false;
+        if (barking  && hourOfDay < 8 || hourOfDay > 22){ return true;
+        } return false;
+    }
+    
+}
